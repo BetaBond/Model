3601357ff55a527a9ee51581b6fce2f4d2e5265f
BetaBond <bond.ryo.beta@gmail.com>
2023-05-30 15:39:24 +0800

    [0.x] 移除测试

---
+++ src/Model.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Colorful\Model;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+/**
+ * 模型基类
+ *
+ * @author beta
+ */
+class Model extends EloquentModel
+{
+    
+    /**
+     * 表名称
+     *
+     * @var string
+     */
+    protected $table = '';
+    
+    /**
+     * 与表相关联的主键
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    
+    /**
+     * 指示模型的ID不是自增的
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    
+    /**
+     * 指示模型是否主动维护时间戳
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    
+    /**
+     * 可以大量分配的属性
+     *
+     * @var array<string>
+     */
+    protected $fillable = [];
+    
+    /**
+     * 应该为序列化而隐藏的属性
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+    
+    /**
+     * 应该强制转换的属性
+     *
+     * @var array
+     */
+    protected $casts = [];
+    
+    /**
+     * 追踪类
+     *
+     * @var Trace
+     */
+    protected Trace $trace;
+    
+    /**
+     * 创建一个新的 [Eloquent] 模型实例
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $trace = $this->trace;
+        
+        $this->fillable = array_values($trace::getAllColumns());
+        $this->table = $trace::TABLE;
+        $this->primaryKey = $trace::ID;
+        $this->hidden = $trace::HIDE;
+        
+        parent::__construct($attributes);
+    }
+    
+    /**
+     * 创建前执行的操作
+     *
+     * @param  Builder  $query
+     * @return bool
+     */
+    protected function performInsert(Builder $query): bool
+    {
+        $trace = $this->trace;
+        
+        if (empty($this->getAttribute($trace::ID))) {
+            $this->setAttribute($trace::ID, time());
+        }
+        
+        $this->setAttribute($trace::CREATED_AT, time());
+        $this->setAttribute($trace::UPDATED_AT, time());
+        
+        return parent::performInsert($query);
+    }
+    
+    /**
+     * 执行模型更新操作
+     *
+     * @param  Builder  $query
+     * @return bool
+     */
+    protected function performUpdate(Builder $query): bool
+    {
+        $trace = $this->trace;
+        
+        $this->setAttribute($trace::UPDATED_AT, time());
+        
+        return parent::performUpdate($query);
+    }
+    
+}
---
+++ src/Trace.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Colorful\Model;
+
+use ReflectionClass;
+
+/**
+ * 追踪 [laravel Eloquent ORM]
+ *
+ * @author beta
+ */
+abstract class Trace
+{
+    
+    /**
+     * 表名称
+     *
+     * @var string
+     */
+    const TABLE = 'table';
+    
+    /**
+     * ID 名称
+     *
+     * @var string
+     */
+    const ID = 'id';
+    
+    /**
+     * 更新时间
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'updated_at';
+    
+    /**
+     * 创建时间
+     *
+     * @var string
+     */
+    const CREATED_AT = 'created_at';
+    
+    /**
+     * 隐藏列
+     *
+     * @var array
+     */
+    const HIDE = [];
+    
+    /**
+     * 获取类所有常量
+     *
+     * @return array
+     */
+    private static function getConstants(): array
+    {
+        return (new ReflectionClass(get_called_class()))
+            ->getConstants();
+    }
+    
+    /**
+     * 获取所有列名称
+     *
+     * @return array
+     */
+    public static function getAllColumns(): array
+    {
+        $constants = self::getConstants();
+        
+        return array_filter($constants, function (string $key) {
+            return !in_array($key, ['TABLE', 'HIDE']);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+    
+}
